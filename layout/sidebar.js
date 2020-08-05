e86ccd28ea0586e156310af3d2b3c4a2fbb5a4ae
Maciek <mstikowski94@gmail.com>
2020-08-05 10:44:41 +0200

    Add sidebar nav button

--- layout/sidebar.js
+++ layout/sidebar.js
@@ -5,9 +5,41 @@ const Wrapper = styled.nav`
   background-color: ${palette.primary.main};
   grid-column: sidebar-start / sidebar-end;
   grid-row: 1 / -1;
+
+  display: flex;
+  justify-content: center;
+`;
+
+const NavButton = styled.button`
+  border: none;
+  border-radius: none;
+
+  background-color: #fff;
+  height: 2px;
+  width: 4.5rem;
+  margin-top: 4rem;
+
+  &::before,
+  &::after {
+    content: '';
+    display: block;
+    background-color: #fff;
+    height: 2px;
+    width: 4.5rem;
+  }
+
+  &::before {
+    transform: translateY(-1.5rem);
+  }
+  &::after {
+    transform: translateY(1.3rem);
+  }
 `;
 
 export default function Sidebar() {
-  return <Wrapper>Sidebar</Wrapper>
+  return (
+    <Wrapper>
+      <NavButton />
+    </Wrapper>
+  );
 }
-
